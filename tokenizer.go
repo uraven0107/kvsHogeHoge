b7a7f589f6d56a849eb2370814b055ff367d739e
uraven0107 <turn.pso@gmail.com>
2021-08-07 23:27:39 +0900

    :pencil: トーカナイザー追加

---
+++ tokenizer.go
@@ -0,0 +1,65 @@
+package main
+
+import "errors"
+
+type Tokenizer struct {
+	tokens []string
+	p      int
+}
+
+func NewTokenizer(source string) *Tokenizer {
+	// 文字列からトークンスライス生成する
+	runes := []rune(source)
+	tokens := make([]string, len(runes)) // ルーンの数 >= 文字列の数
+	str := ""
+	for _, r := range runes {
+		switch s := string(r); s {
+		case "=", "{", "}", ";":
+			if str != "" {
+				tokens = append(tokens, str)
+				str = ""
+			}
+			tokens = append(tokens, s)
+		default:
+			str = str + s
+		}
+	}
+
+	// 空文字以外の文字列数を出す
+	tokens_len := 0
+	for _, t := range tokens {
+		if t == "" {
+			continue
+		}
+		tokens_len = tokens_len + 1
+	}
+
+	// トークンスライスから空文字を除去する
+	new_tokens := make([]string, tokens_len)
+	i := 0
+	for _, t := range tokens {
+		if t == "" {
+			continue
+		}
+		new_tokens[i] = t
+		i = i + 1
+	}
+
+	return &Tokenizer{
+		tokens: new_tokens,
+		p:      0,
+	}
+}
+
+func (t *Tokenizer) next() (string, error) {
+	if !t.hasNext() {
+		return "", errors.New(":( No such Element")
+	}
+	str := t.tokens[t.p]
+	t.p = t.p + 1
+	return str, nil
+}
+
+func (t *Tokenizer) hasNext() bool {
+	return len(t.tokens) >= t.p+1
+}
---
+++ tokenizer_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_next(t *testing.T) {
+	tests := []struct {
+		name  string
+		wants []string
+	}{
+		{
+			name: "canIterateToken",
+			wants: []string{
+				"test",
+				"=",
+				"{",
+				"hoge",
+				"=",
+				"fuga",
+				";",
+				"foo",
+				"=",
+				"bar",
+				";",
+				"}",
+				";",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			source := "test={hoge=fuga;foo=bar;};"
+			tokenizer := NewTokenizer(source)
+			for _, want := range tt.wants {
+				got, err := tokenizer.next()
+				if err != nil {
+					t.Errorf("Error has occured at next(). error = %v", err)
+				}
+				if got != want {
+					t.Errorf("next() = %v, want %v", got, want)
+				}
+			}
+
+		})
+	}
+}
