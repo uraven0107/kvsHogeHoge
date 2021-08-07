e0e46c51d3081c4d9127161b2604d2b2330caced
uraven0107 <turn.pso@gmail.com>
2021-08-07 23:34:14 +0900

    :wrench: fix torkenizer test

--- tokenizer_test.go
+++ tokenizer_test.go
@@ -33,6 +33,9 @@ func Test_next(t *testing.T) {
 			source := "test={hoge=fuga;foo=bar;};"
 			tokenizer := NewTokenizer(source)
 			for _, want := range tt.wants {
+				if !tokenizer.hasNext() {
+					t.Errorf("hasNext() should return true.")
+				}
 				got, err := tokenizer.next()
 				if err != nil {
 					t.Errorf("Error has occured at next(). error = %v", err)
@@ -40,6 +43,9 @@ func Test_next(t *testing.T) {
 				if got != want {
 					t.Errorf("next() = %v, want %v", got, want)
 				}
+			}
+			if tokenizer.hasNext() {
+				t.Errorf("hasNext() should return false.")
 			}
 
 		})
