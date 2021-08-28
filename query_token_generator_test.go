76f76f349553e335a05614957b06cb4073fe0932
uraven0107 <turn.pso@gmail.com>
2021-08-28 23:33:36 +0900

    :wrench: fix Test_QueryTokenGenerate

--- query_token_generator_test.go
+++ query_token_generator_test.go
@@ -9,13 +9,19 @@ import (
 
 func Test_QueryTokenGenerate(t *testing.T) {
 	t.Run("canGenerateQueryToken", func(t *testing.T) {
-		query := "use hoge"
-		expected := []string{"use", "hoge"}
+		assert := assert.New(t)
+		query := "use hoge fuga"
+		expected := []string{"use", "hoge", "fuga"}
 		g := QueryTokenGenerator{}
 		runes := []rune(query)
 		r_p := &runes
 		got := g.Generate(r_p)
-		assert := assert.New(t)
-		assert.Equal(expected, got, fmt.Sprintf("Tokens not equal, expected = %v, but got = %v", expected, got))
+		new_got := []string{}
+		for _, str := range got {
+			if str != "" {
+				new_got = append(new_got, str)
+			}
+		}
+		assert.Equal(expected, new_got, fmt.Sprintf("Tokens not equal, expected = %v, but got = %v", expected, got))
 	})
 }
