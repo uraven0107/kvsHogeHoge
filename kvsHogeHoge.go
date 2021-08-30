e4019f3a22f172dc30006054b5ffc15b26fda2b5
uraven0107 <turn.pso@gmail.com>
2021-08-30 22:42:32 +0900

    :wrench: rename Application to Context

--- kvsHogeHoge.go
+++ kvsHogeHoge.go
@@ -5,23 +5,23 @@ import (
 	"fmt"
 )
 
-type Application struct {
+type Context struct {
 	ds_manager *DatastoreManager
 	current_ds *Datastore
 }
 
-func (app *Application) Current_ds_name() string {
-	if app.current_ds == nil {
+func (ctx *Context) Current_ds_name() string {
+	if ctx.current_ds == nil {
 		return "none"
 	}
-	return app.current_ds.name
+	return ctx.current_ds.name
 }
 
-func (app *Application) Switch_ds(ds_name string) error {
-	ds := app.ds_manager.getDatastore(ds_name)
+func (ctx *Context) Switch_ds(ds_name string) error {
+	ds := ctx.ds_manager.getDatastore(ds_name)
 	if ds == nil {
 		return errors.New(fmt.Sprintf("Datastore named '%v' dosen't exist", ds_name))
 	}
-	app.current_ds = ds
+	ctx.current_ds = ds
 	return nil
 }
--- kvsHogeHoge_test.go
+++ kvsHogeHoge_test.go
@@ -10,14 +10,14 @@ func Test_current_name(t *testing.T) {
 	t.Run("canGetCurrentDatastoreName", func(t *testing.T) {
 		assert := assert.New(t)
 		ds := NewDatastore("hoge")
-		app := &Application{}
-		app.current_ds = ds
-		assert.Equal("hoge", app.Current_ds_name(), "current_name() doesn't equal")
+		ctx := &Context{}
+		ctx.current_ds = ds
+		assert.Equal("hoge", ctx.Current_ds_name(), "current_name() doesn't equal")
 	})
 	t.Run("nilDatastore", func(t *testing.T) {
 		assert := assert.New(t)
-		app := &Application{}
-		assert.Equal("none", app.Current_ds_name(), "current_name() doesn't 'none'")
+		ctx := &Context{}
+		assert.Equal("none", ctx.Current_ds_name(), "current_name() doesn't 'none'")
 	})
 }
 
@@ -29,15 +29,15 @@ func Test_switch_ds(t *testing.T) {
 		ds_list := []*Datastore{ds1, ds2}
 		manager := &DatastoreManager{}
 		manager.ds_list = ds_list
-		app := &Application{}
-		app.ds_manager = manager
-		err := app.Switch_ds("hoge")
+		ctx := &Context{}
+		ctx.ds_manager = manager
+		err := ctx.Switch_ds("hoge")
 		assert.Nil(err, "Switch_ds() shoudn't return error")
-		assert.Equal(ds1, app.current_ds, "current_ds hasn't expected")
-		err = app.Switch_ds("fuga")
+		assert.Equal(ds1, ctx.current_ds, "current_ds hasn't expected")
+		err = ctx.Switch_ds("fuga")
 		assert.Nil(err, "Switch_ds() shouldn't return error")
-		assert.Equal(ds2, app.current_ds, "current_ds hasn't expected")
-		err = app.Switch_ds("unko")
+		assert.Equal(ds2, ctx.current_ds, "current_ds hasn't expected")
+		err = ctx.Switch_ds("unko")
 		assert.Error(err, "Switch_ds() should return error")
 	})
 }
