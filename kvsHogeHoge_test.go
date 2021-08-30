a6d105e2052c76be3131f70ce4ca60a43a66b871
uraven0107 <turn.pso@gmail.com>
2021-08-30 22:31:55 +0900

    :heavy_plus_sign: add Switch_ds()

--- kvsHogeHoge_test.go
+++ kvsHogeHoge_test.go
@@ -10,13 +10,34 @@ func Test_current_name(t *testing.T) {
 	t.Run("canGetCurrentDatastoreName", func(t *testing.T) {
 		assert := assert.New(t)
 		ds := NewDatastore("hoge")
-		app := Application{}
+		app := &Application{}
 		app.current_ds = ds
 		assert.Equal("hoge", app.Current_ds_name(), "current_name() doesn't equal")
 	})
 	t.Run("nilDatastore", func(t *testing.T) {
 		assert := assert.New(t)
-		app := Application{}
+		app := &Application{}
 		assert.Equal("none", app.Current_ds_name(), "current_name() doesn't 'none'")
+	})
+}
+
+func Test_switch_ds(t *testing.T) {
+	t.Run("canSwitchDatastore", func(t *testing.T) {
+		assert := assert.New(t)
+		ds1 := NewDatastore("hoge")
+		ds2 := NewDatastore("fuga")
+		ds_list := []*Datastore{ds1, ds2}
+		manager := &DatastoreManager{}
+		manager.ds_list = ds_list
+		app := &Application{}
+		app.ds_manager = manager
+		err := app.Switch_ds("hoge")
+		assert.Nil(err, "Switch_ds() shoudn't return error")
+		assert.Equal(ds1, app.current_ds, "current_ds hasn't expected")
+		err = app.Switch_ds("fuga")
+		assert.Nil(err, "Switch_ds() shouldn't return error")
+		assert.Equal(ds2, app.current_ds, "current_ds hasn't expected")
+		err = app.Switch_ds("unko")
+		assert.Error(err, "Switch_ds() should return error")
 	})
 }
